68412cc05dfff3663e104f63e92f6ab14f8a7bdd
Joey Mejias <joeymejias@gmail.com>
2015-05-30 15:15:17 -0400

    Initial commit.
    Colors, and buttons, and stuff.
    
    Toast still needs to compare against real time. :(

---
+++ app/src/main/java/com/joeymejias/hello/MainActivity.java
@@ -0,0 +1,38 @@
+package com.joeymejias.hello;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showTime(View v) {
+        Toast toast = Toast.makeText(this, "The time is %s.", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
